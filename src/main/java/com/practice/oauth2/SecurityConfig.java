52234b000af843fcd93004adda0a0ec2bb77f8aa
yunlong.wang <ylwang9627@foxmail.com>
2022-03-24 09:56:33 +0800

    yl #0 oauth2 custom config 3rd client(Take Github as an example)

--- src/main/java/com/practice/oauth2/SecurityConfig.java
+++ src/main/java/com/practice/oauth2/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.practice.oauth2;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,5 +19,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2Login() // open oauth2 login method
                 .loginProcessingUrl("/authorization_code"); // custom login url
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                githubClientRegistration());
+    }
+
+    // custom client registration for github
+    private ClientRegistration githubClientRegistration() {
+        return ClientRegistration.withRegistrationId("github")
+                .clientId("08f97825310277339bf1")
+                .clientSecret("559c77b772ebd235371180d7077867337e35a55a")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .userNameAttributeName("id")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost:8080/authorization_code")
+                .scope("read:user")
+                .authorizationUri("https://github.com/login/oauth/authorize")
+                .tokenUri("https://github.com/login/oauth/access_token")
+                .userInfoUri("https://api.github.com/user")
+                .clientName("Github")
+                .build();
     }
 }
